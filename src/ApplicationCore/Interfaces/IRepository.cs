ce2ccb26c3f90cbd86963a1d92cfe59642e68d36
MUSTAFA YILMAZ <meka.mustafayilmaz@outlook.com>
2022-06-10 14:51:45 +0300

    Entities, repositories

---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,32 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<List<T>> GetAllAsync();
+
+        Task<List<T>> GetAllAsync(ISpecification<T> specification);
+
+        Task<T> AddAsnyc(T entity);
+
+        Task<T> UpdateAsnyc(T entity);
+
+        Task<T> DeleteAsnyc(T entity);
+
+        Task<int> CountAsync(ISpecification<T> specification);
+
+        Task<T> FirstAsync(ISpecification<T> specification);
+
+        Task<T> FirstOrDefaultAsync(ISpecification<T> specification);
+
+    }
+}
