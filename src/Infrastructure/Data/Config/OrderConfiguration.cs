2003244dc0441df91e6fdbd099c03f1898ec10a4
MUSTAFA YILMAZ <meka.mustafayilmaz@outlook.com>
2022-06-22 17:25:51 +0300

    order added

---
+++ src/Infrastructure/Data/Config/OrderConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(x => x.BuyerId).IsRequired();
+
+            builder.OwnsOne<Address>(x => x.ShipToAddress, o =>
+            {
+                o.WithOwner();
+                o.Property(y => y.Street).IsRequired().HasMaxLength(180);
+                o.Property(y => y.City).IsRequired().HasMaxLength(100);
+                o.Property(y => y.State).IsRequired().HasMaxLength(60);
+                o.Property(y => y.Country).IsRequired().HasMaxLength(90);
+                o.Property(y => y.ZipCode).IsRequired().HasMaxLength(18);
+            });
+
+            builder.Navigation(x => x.ShipToAddress).IsRequired();
+        }
+    }
+}
